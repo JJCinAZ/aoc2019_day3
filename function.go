130c68bcb5c17d5bc89f8787af54fdaa846c7d4b
Joseph Cracchiolo <jjc@leapscape.com>
2019-12-04 11:14:00 -0700

    first attempt part 2

--- function.go
+++ function.go
@@ -9,8 +9,13 @@ import (
 	"strings"
 )
 
+type intersection struct {
+	wirebits    int
+	wirelengths [2]int
+}
+
 var (
-	pointMap map[int64]int
+	pointMap map[int64]intersection
 )
 
 func Part1(w http.ResponseWriter, r *http.Request) {
@@ -27,13 +32,30 @@ func Part1(w http.ResponseWriter, r *http.Request) {
 	buildMap(parseLine(wires[0]), 1)
 	buildMap(parseLine(wires[1]), 2)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte(fmt.Sprintf("Shortest is %d", getClosetIntersection())))
+	w.Write([]byte(fmt.Sprintf("Closest is %d", getClosetIntersection())))
+}
+
+func Part2(w http.ResponseWriter, r *http.Request) {
+	wires := make([]string, 0, 2)
+	scanner := bufio.NewScanner(r.Body)
+	for scanner.Scan() {
+		wires = append(wires, scanner.Text())
+	}
+	if len(wires) != 2 {
+		http.Error(w, "Missing input, expect two wire paths", http.StatusNotAcceptable)
+		return
+	}
+	initMap()
+	buildMap(parseLine(wires[0]), 1)
+	buildMap(parseLine(wires[1]), 2)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("Shortest is %d", getShortestIntersection())))
 }
 
 func getClosetIntersection() int {
 	shortest := int32(0)
-	for p, wirebits := range pointMap {
-		if wirebits == 3 && p != 0 {
+	for p, intSect := range pointMap {
+		if intSect.wirebits == 3 && p != 0 {
 			var x, y int32
 			x = int32(p >> 32)
 			y = int32(p & 0xFFFFFFFF)
@@ -48,6 +70,21 @@ func getClosetIntersection() int {
 	return int(shortest)
 }
 
+func getShortestIntersection() int {
+	shortest := 0
+	for p, intSect := range pointMap {
+		if intSect.wirebits == 3 && p != 0 {
+			d := intSect.wirelengths[0] + intSect.wirelengths[1]
+			if shortest == 0 {
+				shortest = d
+			} else if d < shortest {
+				shortest = d
+			}
+		}
+	}
+	return shortest
+}
+
 func abs32(i int32) int32 {
 	if i < 0 {
 		return -i
@@ -60,11 +97,14 @@ func parseLine(input string) []string {
 }
 
 func initMap() {
-	pointMap = make(map[int64]int)
+	pointMap = make(map[int64]intersection)
 }
 
 func buildMap(line []string, wirebit int) error {
-	var curX, curY int32
+	var (
+		curX, curY int32
+		dist       int
+	)
 	for _, m := range line {
 		count, err := strconv.Atoi(m[1:])
 		if err != nil {
@@ -73,34 +113,48 @@ func buildMap(line []string, wirebit int) error {
 		switch m[0] {
 		case 'R':
 			for x := 0; x < count; x++ {
-				markPoint(curX, curY, wirebit)
+				markPoint(curX, curY, wirebit, dist)
 				curX++
+				dist++
 			}
 		case 'L':
 			for x := 0; x < count; x++ {
-				markPoint(curX, curY, wirebit)
+				markPoint(curX, curY, wirebit, dist)
 				curX--
+				dist++
 			}
 		case 'U':
 			for y := 0; y < count; y++ {
-				markPoint(curX, curY, wirebit)
+				markPoint(curX, curY, wirebit, dist)
 				curY++
+				dist++
 			}
 		case 'D':
 			for y := 0; y < count; y++ {
-				markPoint(curX, curY, wirebit)
+				markPoint(curX, curY, wirebit, dist)
 				curY--
+				dist++
 			}
 		}
 	}
 	return nil
 }
 
-func markPoint(x, y int32, wirebit int) {
+func markPoint(x, y int32, wirebit int, dist int) {
+	lenIdx := 0
+	if wirebit == 2 {
+		lenIdx = 1
+	}
 	u := int64(x)<<32 | (int64(y) & 0xFFFFFFFF)
-	if c, found := pointMap[u]; found {
-		pointMap[u] = c | wirebit
+	if intSect, found := pointMap[u]; found {
+		intSect.wirebits |= wirebit
+		if intSect.wirelengths[lenIdx] == 0 {
+			intSect.wirelengths[lenIdx] = dist
+		}
+		pointMap[u] = intSect
 	} else {
-		pointMap[u] = wirebit
+		intSect = intersection{wirebits: wirebit}
+		intSect.wirelengths[lenIdx] = dist
+		pointMap[u] = intSect
 	}
 }
--- function_test.go
+++ function_test.go
@@ -9,14 +9,12 @@ import (
 
 func formatIntersections() string {
 	a := make([]string, 0)
-	fmt.Println("------")
-	for p, wirebits := range pointMap {
-		if wirebits == 3 && p != 0 {
+	for p, intSect := range pointMap {
+		if intSect.wirebits == 3 && p != 0 {
 			var x, y int32
 			x = int32(p >> 32)
 			y = int32(p & 0xFFFFFFFF)
 			a = append(a, fmt.Sprintf("%d,%d", x, y))
-			fmt.Printf("%d, %d = %d: %d\n", x, y, abs32(x)+abs32(y), wirebits)
 		}
 	}
 	sort.Strings(a)
@@ -42,6 +40,31 @@ func Test_buildMap1(t *testing.T) {
 			buildMap(parseLine(tt.args.wire1), 1)
 			buildMap(parseLine(tt.args.wire2), 2)
 			if got := formatIntersections(); got != tt.want {
+				t.Errorf("parseLine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildMap2(t *testing.T) {
+	type args struct {
+		wire1, wire2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{"R8,U5,L5,D3", "U7,R6,D4,L4"}, 30},
+		{"test2", args{"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"}, 610},
+		{"test3", args{"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"}, 410},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			initMap()
+			buildMap(parseLine(tt.args.wire1), 1)
+			buildMap(parseLine(tt.args.wire2), 2)
+			if got := getShortestIntersection(); got != tt.want {
 				t.Errorf("parseLine() = %v, want %v", got, tt.want)
 			}
 		})
